7c8d090a8e340ac7625b51e237d75fa1d5c9250d
siddupatel-00 <siddusurishetti@gmail.com>
2026-08-14 19:02:10 +0530

    Update TURN servers to use TLS over TCP for DPI bypass

--- src/hooks/useWebRTC.js
+++ src/hooks/useWebRTC.js
@@ -13,6 +13,21 @@ const ICE_SERVERS = {
     { urls: 'stun:stun2.l.google.com:19302' },
     { urls: 'stun:stun3.l.google.com:19302' },
     { urls: 'stun:stun4.l.google.com:19302' },
+    {
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turns:openrelay.metered.ca:443?transport=tcp',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    }
   ],
 };
 
